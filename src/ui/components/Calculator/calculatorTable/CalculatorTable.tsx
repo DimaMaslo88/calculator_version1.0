c906700d97a24597be7a62926e6268b0909f8c62
DimaMaslo88 <dmitrij-maslo@mail.ru>
2025-02-03 20:48:07 +0300

    "feat/ button component "

--- src/ui/components/Calculator/calculatorTable/CalculatorTable.tsx
+++ src/ui/components/Calculator/calculatorTable/CalculatorTable.tsx
@@ -1,25 +1,26 @@
 import React, {useState} from 'react';
 import {ButtonNumbers} from "ui/components/Calculator/calculatorTable/buttons/buttonNumbers/ButtonNumbers";
 import style from 'styles/input.module.css'
+
 export const CalculatorTable = () => {
     const [count, setCount] = useState<string>('')
-
-    const onButtonClickHandler=(num:string)=>{
-if(num === '0'){
-    return setCount('0')
-}else{
-    setCount(count+num)
-}
+    const [newCount, setNewCount] = useState<string>('')
+    const onButtonClickHandler = (num: string) => {
+        if (num === '0') {
+            return setCount('0')
+        } else {
+            setCount(count + num)
+        }
 
 
     }
     return (
         <div>
             <div className={style.inputContainer}>
-               <input type='text' value= {count}  readOnly={true} className={style.input}/>
+                <input type='text' value={count} readOnly={true} className={style.input}/>
 
             </div>
-            <ButtonNumbers onChange={(pushNumber)=>onButtonClickHandler(pushNumber)}/>
+            <ButtonNumbers onChange={(pushNumber) => onButtonClickHandler(pushNumber)}/>
 
         </div>
     );
