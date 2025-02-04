6c91c9ac779830694be6f980d4c8077ecdd68960
DimaMaslo88 <dmitrij-maslo@mail.ru>
2025-02-04 22:40:56 +0300

    "fix/ calc bugs "

--- src/ui/components/Calculator/calculatorTable/CalculatorTable.tsx
+++ src/ui/components/Calculator/calculatorTable/CalculatorTable.tsx
@@ -1,15 +1,28 @@
 import React, {useState} from 'react';
 import {ButtonNumbers} from "ui/components/Calculator/calculatorTable/buttons/buttonNumbers/ButtonNumbers";
 import style from 'styles/input.module.css'
+import {buttonsArray} from "state/store";
 
 export const CalculatorTable = () => {
     const [count, setCount] = useState<string>('')
-    const [newCount, setNewCount] = useState<string>('')
+    const [result, setResult] = useState<string>('0')
+
     const onButtonClickHandler = (num: string) => {
         if (num === '0') {
-            return setCount('0')
+            setCount('0')
         } else {
             setCount(count + num)
+        }
+
+        if (num) {
+            // return alert("Hello")
+           return  setResult(eval(count + num).toString())
+        }
+    }
+    const onSpecialSymbolsClickHandler = (symbol: string) => {
+        if (symbol === 'AC') {
+            setCount('')
+            setResult('0')
         }
 
 
@@ -18,9 +31,10 @@ export const CalculatorTable = () => {
         <div>
             <div className={style.inputContainer}>
                 <input type='text' value={count} readOnly={true} className={style.input}/>
-
+                {result}
             </div>
-            <ButtonNumbers onChange={(pushNumber) => onButtonClickHandler(pushNumber)}/>
+            <ButtonNumbers onChange={(pushNumber) => onButtonClickHandler(pushNumber)}
+                           onChangeSymbols={onSpecialSymbolsClickHandler}/>
 
         </div>
     );
--- src/ui/components/Calculator/calculatorTable/buttons/buttonNumbers/ButtonNumbers.tsx
+++ src/ui/components/Calculator/calculatorTable/buttons/buttonNumbers/ButtonNumbers.tsx
@@ -5,8 +5,9 @@ import {buttonsArray, specialButtons} from "state/store";
 import {ButtonsArrayType, SpecialButtonsType} from "types/types";
 type ButtonNumbersType={
     onChange:(num:string)=>void
+    onChangeSymbols:(sym:string)=>void
 }
-export const ButtonNumbers = ({onChange}:ButtonNumbersType) => {
+export const ButtonNumbers = ({onChange,onChangeSymbols}:ButtonNumbersType) => {
 
     // const numbers =[]
     // for(let i=0;i<=9;i+=1){
@@ -16,10 +17,12 @@ export const ButtonNumbers = ({onChange}:ButtonNumbersType) => {
     return (
         <div className={style.buttonsContainer} >
             {specialButtons.map(({id,title}:SpecialButtonsType)=>(
-                <button
+
+                <button onClick={()=>onChangeSymbols(title)}
                     key={id}
                 >{title}</button>
             ))}
+
             {buttonsArray.map(({id,title}:ButtonsArrayType)=>(
 
                 <button
